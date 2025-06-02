6d3dfc341790aafa562afe384736b9a82c426996
YNA store <nadbrtpro@gmail.com>
2025-06-03 02:33:23 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-document.querySelectorAll('a').forEach(link => {
-  link.addEventListener('click', () => {
-    const audio = new Audio('https://www.myinstants.com/media/sounds/pop-cat-original-meme.mp3');
-    audio.play();
-  });
-});
